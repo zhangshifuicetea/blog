6072d8c923a9416f9fe2f88a0c9130db30c63eb6
zhangshifu <tiejiayrz@outlook.com>
2020-03-28 21:16:02 +0800

    config

--- src/app/store.ts
+++ src/app/store.ts
@@ -13,4 +13,4 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk = ThunkAction<void, RootState, unknown, Action<string>>;
 export type AppDispatch = typeof store.dispatch;
-//
+
