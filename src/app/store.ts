c6467d622ce89050cfb7e46690e1da01bacea146
zhangshifu <tiejiayrz@outlook.com>
2020-03-28 21:12:23 +0800

    config

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,15 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import { combineReducers } from '@reduxjs/toolkit'
+
+const rootReducer = combineReducers({
+  counter: counterReducer,
+});
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer: rootReducer
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk = ThunkAction<void, RootState, unknown, Action<string>>;
+export type AppDispatch = typeof store.dispatch;
